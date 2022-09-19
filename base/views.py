2c9d5d6442f4b3b31830e66019ed7c18681c0aeb
salma-achour <salma.achour@esprit.tn>
2022-09-19 20:40:30 +0100

    templates part2

--- base/views.py
+++ base/views.py
@@ -1,9 +1,21 @@
+from multiprocessing import context
 from django.shortcuts import render
 
+rooms = [
+    {'id':1, 'name':'Let\'s Learn Python'},
+    {'id':2, 'name':'AI Room'},
+    {'id':3, 'name':'Design with me'}
+]
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    context = {'rooms': rooms}
+    return render(request, "base/home.html", context)
 
-def room(request):
-    return  render(request, "room.html")
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {'room': room}
+    return  render(request, "base/room.html", context)
