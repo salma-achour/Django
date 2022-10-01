368680aea8fb68c1840e21c174997b619e51b50a
salma-achour <salma.achour@esprit.tn>
2022-10-01 13:03:19 +0100

    static files and fixes

--- django_tuto/settings.py
+++ django_tuto/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'  # let django know that a=our static files are hosted in /static
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
