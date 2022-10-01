c050a5f7e7c3864bd74402f3309bec82eb8082ff
salma-achour <salma.achour@esprit.tn>
2022-10-01 11:15:43 +0100

    message functionality

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from http.client import HTTPResponse
 from multiprocessing import context
 from django.shortcuts import redirect, render
 from django.contrib import messages
@@ -6,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 
@@ -79,7 +80,18 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by("-created")
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    
+    context = {'room': room, 'room_messages':room_messages, 'participants':participants}
     return  render(request, "base/room.html", context)
 
 @login_required(login_url='login')
@@ -121,3 +133,13 @@ def delete_room(request, pk):
     return render(request, "base/delete.html", {"obj": room})
     
     
+@login_required(login_url="login")
+def delete_message(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        HTTPResponse('You are not allowed here')
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':message})
+        
